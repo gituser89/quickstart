758a19ba3508c0ea83ce4a7f67ff3b563e9e9e21
Z003RNYJ <Z003RNYJ@AAEINBLR03598L.ad001.siemens.net>
2017-07-12 16:49:10 +0530

    # WARNING: head commit changed in the meantime
    Files Added

---
+++ Test1/src/Tesit.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Tesit {
+	
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+	    System.out.println("Enter the String & Number :");
+	    String name = in.nextLine();
+	    int numbers = in.nextInt();
+	    System.out.println("Name :"+ name + "Number :" +numbers);
+	}
+
+}
